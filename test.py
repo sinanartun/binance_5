23c4739babe6f887f990040cd27025dacc4946be
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-06-01 18:15:08 +0200

    update

--- test.py
+++ test.py
@@ -1,35 +1,11 @@
-import json
-
-import boto3
-
-s3 = boto3.client('s3')
-res = s3.list_buckets()
-print(json.dumps(res, indent=4, sort_keys=True, default=str))
-
-#
-# url_token = "http://169.254.169.254/latest/api/token"
-# url_meta_data = "http://169.254.169.254/latest/meta-data/iam/info"
-# headers = {"X-aws-ec2-metadata-token-ttl-seconds": "21600"}
-#
-# # Request to get the token
-# response = requests.put(url_token, headers=headers)
-#
-# if response.status_code == 200:
-#     token = response.text
-#     headers["X-aws-ec2-metadata-token"] = token
-#
-#     # Request to get the meta-data
-#     response = requests.get(url_meta_data, headers=headers)
-#
-#     s3 = boto3.client(
-#         's3'
-#     )
-#     res = s3.list_buckets()
-#     print(json.dumps(res, indent=4, sort_keys=True, default=str))
-#
-#     if response.status_code == 200:
-#         print(response.text)
-#     else:
-#         print(f"Error: Unable to fetch meta-data. HTTP status code: {response.status_code}")
-# else:
-#     print(f"Error: Unable to get token. HTTP status code: {response.status_code}")
+{"Records": [
+    {"eventVersion": "2.1", "eventSource": "aws:s3", "awsRegion": "eu-west-1", "eventTime": "2023-05-31T18:37:19.458Z",
+     "eventName": "ObjectCreated:Put", "userIdentity": {"principalId": "A1QTDEYAXHWOBD"},
+     "requestParameters": {"sourceIPAddress": "92.34.149.230"},
+     "responseElements": {"x-amz-request-id": "G5AZFRJ2NQHSQBV8",
+                          "x-amz-id-2": "zeK+3iS+Y1pXUoJPWuYZRIcRS2rF6Qg2MijzPgdoZVpPozGEMZ09WwZJQF0Uar9kSituzCc44vnjnrtr5+c+5zWhf9e4CFA2"},
+     "s3": {"s3SchemaVersion": "1.0", "configurationId": "8b64a1dd-0fe1-47f8-b96e-6adc09411e99",
+            "bucket": {"name": "awsbc5-irish", "ownerIdentity": {"principalId": "A1QTDEYAXHWOBD"},
+                       "arn": "arn:aws:s3:::awsbc5-irish"},
+            "object": {"key": "data_1_min/1685556360_new.tsv", "size": 7250, "eTag": "11bd579bf8d10dcd0eefb1bb7729a099",
+                       "sequencer": "00647793DF6B6AAB2D"}}}]}
